77a567a07873d60fd35f8d89be3d43aff5dd78c1
TyrusB <brooks85.ty@gmail.com>
2015-01-25 01:16:42 -0800

    Refactor cels to fahr to use a helper function

--- ch1/celsius_to_fahr_improved.c
+++ ch1/celsius_to_fahr_improved.c
@@ -4,11 +4,15 @@
 #define UPPER   80 /* Upper limit of table */
 #define STEP    5  /* step size */
 
+float cels_to_fahr( int celsius );
+
 main() {
   int celsius;
   
   for (celsius = LOWER; celsius <= UPPER; celsius += STEP)
-    printf("%3d %6.1f\n", celsius, (9.0/5.0) * celsius + 32);
+    printf("%3d %6.1f\n", celsius, cels_to_fahr( celsius ) );
 }
   
-  
+float cels_to_fahr( int celsius ) {
+  return (9.0/5.0) * celsius + 32;
+}
